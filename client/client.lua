8886a728803a6a53acddb871180112a46335588b
Giovanni M Russo <turangarusso@gmail.com>
2023-09-25 20:14:34 +0200

    Update client.lua
    Fiveguard.net

--- client/client.lua
+++ client/client.lua
@@ -1,3 +1,14 @@
+
+
+-- fiveguard.net
+
+-- For better paid anticheat check out https://store.fiveguard.ac/.
+
+-- We are able to provide this free product because of help of https://fiveguard.net/.
+
+-- Fiveguard is best paid fivem anticheat
+
+
 local isSpawn = false
 AddEventHandler("playerSpawned", function()
     isSpawn = true
